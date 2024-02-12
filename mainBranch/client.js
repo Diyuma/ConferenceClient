365cbb112852751e63f8a9a660eda828609deeee
Алексей Маренков <lehatrutenb@gmail.com>
2024-02-12 17:13:37 +0300

    Update codebase even with some debug info

--- mainBranch/client.js
+++ mainBranch/client.js
@@ -274,15 +274,31 @@ async function sendSound(request) {
     });
 }
 
+let TO_SEND = []; // TODO change variable name
+let GRAIN_SERVER_DURATION = 256; // TODO GET THAT PARAM FROM SERVER!!!!!!!!!!! // in ms
+
 async function sendAudioBlobDataToServer(blob, bitRate) {
-    var request = new ChatClientMessage();
-    request.setRate(bitRate);
-    request.setDataList(await serverBytesWavDataToWaveArray(blob, bitRate));
+    TO_SEND.push(...(await serverBytesWavDataToWaveArray(blob, bitRate)));
 
-    request.setUserid(userId);
-    request.setConfid(confId);
+    let step = (GRAIN_SERVER_DURATION * bitRate) / 1024; // / 1024 because sd in ms but we don't really need seconds
+    let lastRightIndex = 0;
+    for (let i = step; i <= TO_SEND.length; i += step) {
+        async function sendRequest(dataToSend) {
+            var request = new ChatClientMessage();
+            request.setRate(bitRate);
+            request.setDataList(dataToSend);
+            request.setUserid(userId);
+            request.setConfid(confId);
 
-    sendSound(request);
+            for (let j = 0; j < 100; j++) { // remove it !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                sendSound(request);
+            }
+        }
+
+        sendRequest(TO_SEND.slice(i - step, i));
+        lastRightIndex = i;
+    }
+    TO_SEND = TO_SEND.slice(lastRightIndex, TO_SEND.length);
 }
 
 async function initNewClient() {
---
+++ mainBranch/proto/proto_pb2.pyi
@@ -0,0 +1,60 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ChatServerMessage(_message.Message):
+    __slots__ = ("data", "rate", "soundId")
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    RATE_FIELD_NUMBER: _ClassVar[int]
+    SOUNDID_FIELD_NUMBER: _ClassVar[int]
+    data: _containers.RepeatedScalarFieldContainer[float]
+    rate: int
+    soundId: int
+    def __init__(self, data: _Optional[_Iterable[float]] = ..., rate: _Optional[int] = ..., soundId: _Optional[int] = ...) -> None: ...
+
+class ChatClientMessage(_message.Message):
+    __slots__ = ("data", "rate", "userId", "confId")
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    RATE_FIELD_NUMBER: _ClassVar[int]
+    USERID_FIELD_NUMBER: _ClassVar[int]
+    CONFID_FIELD_NUMBER: _ClassVar[int]
+    data: _containers.RepeatedScalarFieldContainer[float]
+    rate: int
+    userId: int
+    confId: int
+    def __init__(self, data: _Optional[_Iterable[float]] = ..., rate: _Optional[int] = ..., userId: _Optional[int] = ..., confId: _Optional[int] = ...) -> None: ...
+
+class ClientResponseMessage(_message.Message):
+    __slots__ = ("rate", "soundId")
+    RATE_FIELD_NUMBER: _ClassVar[int]
+    SOUNDID_FIELD_NUMBER: _ClassVar[int]
+    rate: int
+    soundId: int
+    def __init__(self, rate: _Optional[int] = ..., soundId: _Optional[int] = ...) -> None: ...
+
+class ClientInfoMessage(_message.Message):
+    __slots__ = ("confId", "userId")
+    CONFID_FIELD_NUMBER: _ClassVar[int]
+    USERID_FIELD_NUMBER: _ClassVar[int]
+    confId: int
+    userId: int
+    def __init__(self, confId: _Optional[int] = ..., userId: _Optional[int] = ...) -> None: ...
+
+class ClientUserInitResponseMessage(_message.Message):
+    __slots__ = ("userId",)
+    USERID_FIELD_NUMBER: _ClassVar[int]
+    userId: int
+    def __init__(self, userId: _Optional[int] = ...) -> None: ...
+
+class ClientConfInitResponseMessage(_message.Message):
+    __slots__ = ("confId",)
+    CONFID_FIELD_NUMBER: _ClassVar[int]
+    confId: int
+    def __init__(self, confId: _Optional[int] = ...) -> None: ...
+
+class EmptyMessage(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
