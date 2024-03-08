08d1c075ef8ae45afbcd6dd39c7481885331c497
Алексей Маренков <lehatrutenb@gmail.com>
2024-03-09 00:47:38 +0300

    Fix issue with too fast client ; fix issue with deque - moved to arr ; make code more readable ; may be fix  issue write and play not sync

--- html/client.js
+++ html/client.js
@@ -54,29 +54,27 @@ function initSoundPlayer() {
     let readyAmt = 0;
     let LastBufferIndex = 0;
     let lastWaveValues = [];
-    let soundLen = 5; // in seconds
-    let safeLen = 1;
+    let SOUND_LEN = 5; // in seconds
+    let SAFE_ZONE_SZ = 2;
+    //let safeLen = 1;
     let lastsr = -100;
 
     //let audioCtxs = [[], []];
-    let buffers = [null, null, null, null, null, null, null, null, null, null];
-    let nowPlayingInd = 0;
+    let buffers = [];
+    let playingRates = [];
+    createNewBuffer(0);
+    createNewBuffer(1);
+
+    let nowPlayingInd = -1;
     let nowWritingInd = 0;
 
-    let audioCtxs = [new AudioContext(), new AudioContext()];
-    let sources = [audioCtxs[0].createBufferSource(), audioCtxs[1].createBufferSource()];
-    let playingRates = [nextRecorderBitRate, nextRecorderBitRate, nextRecorderBitRate, nextRecorderBitRate, nextRecorderBitRate, nextRecorderBitRate, nextRecorderBitRate, nextRecorderBitRate, nextRecorderBitRate, nextRecorderBitRate];
-
-    sources[0].connect(audioCtxs[0].destination);
-
-    buffers[0] = prepareSoundPlayer(nextRecorderBitRate);
-
-    sources[0].buffer = buffers[0];
+    let audioCtxs = [new AudioContext()];
+    let sources = [];
 
     function prepareSoundPlayer(bitRate) {
         let channels = 1;
 
-        const frameCount = bitRate * (soundLen + safeLen);
+        const frameCount = bitRate * SOUND_LEN;
 
         let buffer = new AudioBuffer({
             numberOfChannels: channels,
@@ -87,23 +85,36 @@ function initSoundPlayer() {
         return buffer;
     }
 
-    async function startPlayingAndPrepareNext(nextPlayingInd) { // какой-то мега баг - playing ind то ли по модулю 2 то ли 10 (len(buffers))
-        sources[(nextPlayingInd + 1) % 2].start();
-        audioCtxs[nextPlayingInd] = new AudioContext();
-        sources[nextPlayingInd] = audioCtxs[nextPlayingInd].createBufferSource();
+    function clientIsTooFastPlaying() {
+        if (nextRecorderBitRate > startBitRate) {
+            nextRecorderBitRate /= 2;
+            nextBitRateInd -= 1;
+        }
+        nowPlayingInd--;
+        readyAmt = 0; // to make client wait next x server records
+    }
+
+    async function startPlayingAndPrepareNext(nextPlayingInd) {
+        sources[nextPlayingInd - 1].start();
         
         let toSleep = 1;
-        if (nowPlayingInd )
-        setTimeout(() => startPlayingAndPrepareNext((nextPlayingInd + 1) % 2), (soundLen + toSleep) * 1000);
 
-        sources[nextPlayingInd].connect(audioCtxs[nextPlayingInd].destination);
-        nowPlayingInd = (nowPlayingInd + 1) % buffers.length;
-        if (buffers[nowPlayingInd] == null) {
-            buffers[nowPlayingInd] = prepareSoundPlayer(nextRecorderBitRate);
+        if (nextPlayingInd > nowWritingInd + 1) {
+            clientIsTooFastPlaying();
+            SAFE_ZONE_SZ++;
+            return;
         }
-        sources[nextPlayingInd].buffer = buffers[nowPlayingInd];
 
-        buffers[(nowPlayingInd - 3 + buffers.length) % buffers.length] = null;
+        nowPlayingInd = nextPlayingInd - 1;
+        if (nextPlayingInd >= 3) {
+            buffers[nextPlayingInd - 3] = null; // TODO I believe that -2 is enough , but -3 just to be 100% sure
+        }
+        setTimeout(() => startPlayingAndPrepareNext(nextPlayingInd + 1), SOUND_LEN * 1000);
+
+        audioCtxs[nextPlayingInd] = new AudioContext();
+        sources[nextPlayingInd] = audioCtxs[nextPlayingInd].createBufferSource();
+        sources[nextPlayingInd].connect(audioCtxs[nextPlayingInd].destination);
+        sources[nextPlayingInd].buffer = buffers[nextPlayingInd];
     }
 
     function getSoundIndInCurBR(ind, frBR, toBR) {
@@ -114,10 +125,54 @@ function initSoundPlayer() {
         return (ind - (ind % k)) / k;
     }
 
+    function SetDataToBuffer(li, amt, toAdd, toAddBR, buf) {
+        if (mySound.has(toAdd[2]) && NOT_TESTING) {
+            let toDecrim = mySound.get();
+            let toDecrimBR = mySoundBR.get(toAdd[2]);
+
+            for (let i = li; i < li + amt; i++) {
+                buf[i - li + LastBufferIndex] = toAdd[0][getSoundIndInCurBR(i, toAddBR, playingRates[nowWritingInd])] - toDecrim[getSoundIndInCurBR(i, toDecrimBR, playingRates[nowWritingInd])];
+            }
+        } else {
+            /*if (lastsr != -100) {
+                for (let i = 0; i < 100; i++) {
+                    //nowBuffering[i + LastBufferIndex] = (lastsr * (100 - i) / 100.0 + toAdd[0][i] * (i + 1) / 100);
+                    nowBuffering[i + LastBufferIndex] = (lastsr * (100 - i) / 100.0 + toAdd[0][i])  / 2.0;
+                }
+
+                for (let i = 100; i < toAdd[0].length; i++) {
+                    nowBuffering[i + LastBufferIndex] = toAdd[0][i];
+                }
+            } else {
+                for (let i = 0; i < toAdd[0].length; i++) {
+                    nowBuffering[i + LastBufferIndex] = toAdd[0][i];
+                }
+            }*/ // TODO return later - makes sound better connect
+            for (let i = li; i < li + amt; i++) {
+                buf[i - li + LastBufferIndex] = toAdd[0][getSoundIndInCurBR(i, toAddBR, playingRates[nowWritingInd])];
+            }
+            
+
+            /*lastsr = 0;
+            for (let i = 0; i < 100; i++) {
+                lastsr += (toAdd[0][toAdd[0].length - 1 - i] * (100 - i) / 100.0) / 100.0;
+            }*/  // TODO return later - makes sound better connect
+        }
+    }
+
+    function createNewBuffer(ind) {
+        if (TO_RECORD) {
+            console.log("Care, only recording to file");
+            saveFloat32ArrayAsText(nowBuffering, 'data.txt');
+        }
+
+        buffers[ind] = prepareSoundPlayer(nextRecorderBitRate); // TODO it's good idea to just use same bitrate as recorder, isn't it?
+        playingRates[ind] = nextRecorderBitRate;
+    }
+
     async function waitForAudio() { // we do not need such timeouts, later may make them larger
     
         if (audioDeque.length != 0) {
-            console.log("****************", nowPlayingInd, nowWritingInd);
             let toAdd = audioDeque.shift();
             let toAddBR = toAdd[1];
             let bitRateInd = getBitRateInd(toAddBR);
@@ -130,7 +185,7 @@ function initSoundPlayer() {
                 toAddLength /= toAddBR / playingRates[nowWritingInd];
             }
 
-            if (mySound.has(toAdd[2]) && NOT_TESTING) {
+            /*if (mySound.has(toAdd[2]) && NOT_TESTING) {
                 let toDecrim = mySound.get(toAdd[2]);
                 let toDecrimBR = mySoundBR.get(toAdd[2]);
 
@@ -151,92 +206,43 @@ function initSoundPlayer() {
                     for (let i = 0; i < toAdd[0].length; i++) {
                         nowBuffering[i + LastBufferIndex] = toAdd[0][i];
                     }
-                }*/ // TODO return later - makes sound better connect
+                }* / // TODO return later - makes sound better connect
                 for (let i = 0; i < toAddLength; i++) {
                     nowBuffering[i + LastBufferIndex] = toAdd[0][getSoundIndInCurBR(i, toAddBR, playingRates[nowWritingInd])];
-                }
+                }*/
                 
 
                 /*lastsr = 0;
                 for (let i = 0; i < 100; i++) {
                     lastsr += (toAdd[0][toAdd[0].length - 1 - i] * (100 - i) / 100.0) / 100.0;
                 }*/  // TODO return later - makes sound better connect
-            }
-    
-            LastBufferIndex += toAddLength;
-            if (LastBufferIndex >= soundLen * playingRates[nowWritingInd]) {
-                if (TO_RECORD) {
-                    console.log("Care, only recording to file");
-                      saveFloat32ArrayAsText(nowBuffering, 'data.txt');
-                }
 
-                LastBufferIndex = 0;
-                nowWritingInd = (nowWritingInd + 1) % buffers.length;
-                if (buffers[nowWritingInd] == null) {
-                    buffers[nowWritingInd] = prepareSoundPlayer(nextRecorderBitRate); // TODO it's good idea to just use same bitrate as recorder, isn't it?
+            var lastInd = 0;
+            while (lastInd < toAddLength) {
+                var amt = Math.min(toAddLength, SOUND_LEN * playingRates[nowWritingInd] - LastBufferIndex - 1);
+                SetDataToBuffer(lastInd, amt, toAdd, toAddBR, nowBuffering);
+                lastInd += amt;
+                LastBufferIndex += amt;
+
+                if (LastBufferIndex >= SOUND_LEN * playingRates[nowWritingInd] - 1) {
+                    LastBufferIndex = 0;
+                    nowWritingInd += 1;
+                    createNewBuffer(nowWritingInd + 1); // create next buffer
                 }
-                playingRates[nowWritingInd] = nextRecorderBitRate;
             }
     
             readyAmt += 1;
 
-            if (readyAmt == 5) { // 4 not 5 because first floats will be 0
+            if (readyAmt == SAFE_ZONE_SZ) {
                 if (!TO_RECORD) {
-                    startPlayingAndPrepareNext(1);
+                    audioCtxs[nowPlayingInd + 1] = new AudioContext();
+                    sources[nowPlayingInd + 1] = audioCtxs[nowPlayingInd + 1].createBufferSource();
+                    sources[nowPlayingInd + 1].connect(audioCtxs[nowPlayingInd + 1].destination);
+                    sources[nowPlayingInd + 1].buffer = buffers[nowPlayingInd + 1];
+
+                    startPlayingAndPrepareNext(nowPlayingInd + 2);
                 }
-                //const source = audioCtx.createBufferSource();
-                //source.connect(audioCtx.destination);
-
-                    /*buffers[nowPlayingInd^1][1] = prepareSoundPlayer(4096 * 2);
-                    audioCtxs[nowPlayingInd^1] = new AudioContext();
-
-                    sources[nowPlayingInd^1] = audioCtxs[nowPlayingInd^1].createBufferSource();
-
-                    sources[nowPlayingInd^1].connect(audioCtxs[nowPlayingInd^1].destination);
-                    sources[nowPlayingInd^1].buffer = buffers[nowPlayingInd^1][1];*/
             }
-            /*if (readyAmt > 4 && readyAmt % 20 == 5 || readyAmt % 20 == 0) { // 4 not 5 because first floats will be 0
-                let ind = 0;
-                
-                //const source = audioCtx.createBufferSource();
-                //source.connect(audioCtx.destination);
-                if (readyAmt % 20 == 5) {
-                    sources[nowPlayingInd].start();
-
-
-
-
-                    buffers[nowPlayingInd^1][1] = prepareSoundPlayer(4096 * 2);
-                    audioCtxs[nowPlayingInd^1] = new AudioContext();
-
-                    sources[nowPlayingInd^1] = audioCtxs[nowPlayingInd^1].createBufferSource();
-
-                    sources[nowPlayingInd^1].connect(audioCtxs[nowPlayingInd^1].destination);
-                    sources[nowPlayingInd^1].buffer = buffers[nowPlayingInd^1][1];
-                } else {
-                    LastBufferIndex = 0;
-                    nowPlayingInd ^= 1;
-
-                    buffers[nowPlayingInd][1] = prepareSoundPlayer(4096 * 2);
-                    audioCtxs[nowPlayingInd] = new AudioContext();
-
-                    sources[nowPlayingInd] = audioCtxs[nowPlayingInd].createBufferSource();
-
-                    sources[nowPlayingInd].connect(audioCtxs[nowPlayingInd].destination);
-                    sources[nowPlayingInd].buffer = buffers[nowPlayingInd][1];
-
-                    //sources[nowPlayingInd].start();
-                }
-                /*for (let i = startBitRate; i <= maxBitRate; i *= 2) {
-                    /*[audioCtxs[nowPlayingInd][ind], buffers[nowPlayingInd][ind]] = prepareSoundPlayer(i);
-                    const source = audioCtxs[nowPlayingInd][ind].createBufferSource();
-                    source.buffer = buffers[nowPlayingInd][ind];
-                    source.connect(audioCtxs[nowPlayingInd][ind].destination);
-                    source.start();
-
-                    ind++;
-                }* /
-            }*/
             setTimeout(() => waitForAudio(), 50);
         } else {
             setTimeout(() => waitForAudio(), 100);
@@ -281,7 +287,6 @@ async function getSound(confId, userId) {
     var stream = client.getSound(msg, {"Access-Control-Allow-Origin": "*"});
     stream.on('data', function(response) {
         //(response.getDataList(), response.getRate(), response.getSoundid());
-        console.log(response);
         audioDeque.push([response.getDataList(), response.getRate(), response.getSoundid()]);
     });
 }
@@ -310,6 +315,7 @@ async function sendSound(request) {
 
 let TO_SEND = []; // TODO change variable name
 let GRAIN_SERVER_DURATION = 256; // TODO GET THAT PARAM FROM SERVER!!!!!!!!!!! // in ms
+let SEND_STEP_DURATION = 1024;
 let SendMessageIndex = 0;
 
 async function sendAudioBlobDataToServer(blob, bitRate) {
@@ -319,7 +325,7 @@ async function sendAudioBlobDataToServer(blob, bitRate) {
     }*/ //  do not need it - may delete
     TO_SEND.push(...(await serverBytesWavDataToWaveArray(blob, bitRate)));
 
-    let step = (GRAIN_SERVER_DURATION * bitRate) / 1024; // / 1024 because sd in ms but we don't really need seconds
+    let step = (GRAIN_SERVER_DURATION * bitRate) / SEND_STEP_DURATION; // /  SEND_STEP_DURATION because sd in ms but we don't really need seconds
     let lastRightIndex = 0;
     for (let i = step; i <= TO_SEND.length; i += step) {
         async function sendRequest(dataToSend, dataId) {
@@ -330,12 +336,10 @@ async function sendAudioBlobDataToServer(blob, bitRate) {
             request.setConfid(confId);
             request.setTimestamp(Date.now());
             request.setMessageind(dataId);
-            console.log("send", dataId);
 
             sendSound(request);
         }
 
-        console.log("func", SendMessageIndex);
         sendRequest(TO_SEND.slice(i - step, i), (i / step) - 1); // CHANGE TODO!!!!!!! (i / step) - 1 -> iSendMessageIndex
         SendMessageIndex += 1;
         lastRightIndex = i;
@@ -495,6 +499,9 @@ function changeConnectionToConferenceElems(ConferenceId) {
     var st = document.getElementById('stopRecording');
     st.removeAttribute("hidden");
 
+    var cb = document.getElementById('CopyConfIdButton');
+    cb.removeAttribute("hidden");
+
     const confIdText = document.createTextNode("Your conference id is:" + ConferenceId.toString());
     document.body.appendChild(confIdText);
 }
@@ -522,4 +529,8 @@ $("#StartConferenceButton").click(() => {
 
 $("#ConnectToConferenceButton").click(() => {
     createClient();
+});
+
+$("#CopyConfIdButton").click(() => {
+    navigator.clipboard.writeText(confId);
 });
